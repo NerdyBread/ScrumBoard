45598af903dc1a883afb09338e3ee87e72ebbb1d
Sam Fondacaro <samfondacaro2006@gmail.com>
2024-01-07 12:26:27 -0500

    Cleaned up test suite imports

--- Tests/test_basic.py
+++ Tests/test_basic.py
@@ -2,9 +2,7 @@ import sys
 import os
 
 source_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..")
-print(source_path)
 sys.path.insert(0, source_path)
-print(sys.path[0])
 
 import pytest
 
